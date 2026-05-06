4e5914a13092c5158ae8f042051ed3253df7463f
Estudiante <jurodriguezvar@unal.edu.co>
2026-05-06 08:15:09 -0500

    se añadio carpeta input

--- labgit.cpp
+++ labgit.cpp
@@ -1,7 +1,20 @@
-#inlcude <iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
 int main(){
+	ifstream archivoEntrada("input/archivoEntrada.txt");
+	if (archivoEntrada.is_open()) {
+		cout <<"Archivo abierto"<< endl;
+		string linea;
+		
+		while (getline(archivoEntrada, linea)){
+		}
+	}
+	
+
 }
+
 
