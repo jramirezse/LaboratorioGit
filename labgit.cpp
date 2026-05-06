4afb70e7d9dc111c0572c4bf366671e8af151a1e
Estudiante <sjimenezpo@unal.edu.co>
2026-05-06 07:39:00 -0600

    Se añadieron los archivos finales

--- labgit.cpp
+++ labgit.cpp
@@ -6,12 +6,16 @@ using namespace std;
 
 int main(){
 	ifstream archivoEntrada("input/archivoEntrada.txt");
-	if (archivoEntrada.is_open()) {
+	ofstream archivoSalida("output/archivoSalida.txt");
+	
+	if (archivoEntrada.is_open() && archivoSalida.is_open()){
 		cout <<"Archivo abierto"<< endl;
 		string linea;
 		
 		while (getline(archivoEntrada, linea)){
+			archivoSalida <<linea;
 		}
+		cout<<"Archivo copiado correctamente"<<endl;
 	}
 	
 
